03f01f6332ac76f8944b2375e3fd26c0820da097
Erwan Martin <contact@erwanmartin.fr>
2014-09-01 00:50:25 +0200

    [init] add category list and category detail pages

---
+++ components/bookmarks-category-card/bookmarks-category-card.js
@@ -0,0 +1,13 @@
+Polymer('bookmarks-category-card', {
+  ready : function() {
+    var _this = this;
+
+    this.categoryLink = this.shadowRoot.querySelector('a');
+    this.categoryLink.addEventListener('click', function(e){
+      e.preventDefault();
+      _this.fire('category-selected', {
+        name : _this.category.name
+      })
+    });
+  }
+});
--- components/bookmarks-login/bookmarks-login.js
+++ components/bookmarks-login/bookmarks-login.js
@@ -6,45 +6,11 @@ Polymer('bookmarks-login', {
     this.tokenInput   = this.shadowRoot.getElementById('token');
     this.submitButton = this.shadowRoot.getElementById('submit');
 
-    this.addEventListener('init', function(){
-      _this.init.call(_this);
-    })
-  },
-  init : function() {
-    var _this = this;
-    var token = this.getToken.call();
-
-    if (token) {
-
-      this.testToken.call(this, token);
-
-    } else {
-      
-      this.submitButton.addEventListener('click', function(){
-        var token = _this.tokenInput.value;
-        _this.testToken.call(_this, token);
+    this.submitButton.addEventListener('click', function(){
+      var token = _this.tokenInput.value;
+      _this.fire('submit-token', {
+        token : token
       });
-
-      this.displayForm = true;
-
-    }
-  },
-  testToken : function(token) {
-    this.setToken.call(this, token);
-    this.fire('logged', {
-      token : token
     });
-  },
-  getToken : function() {
-    var token = null;
-    if (typeof localStorage!='undefined') {
-      token = localStorage.getItem('token');
-    }
-    return token;
-  },
-  setToken : function(token) {
-    if (typeof localStorage!='undefined') {
-      localStorage.setItem('token', token);
-    }
   }
 });
